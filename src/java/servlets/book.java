29c39e985d2082234077451b2d826b7c6491817a
Samuel Walladge <samuel@swalladge.id.au>
2017-03-14 16:30:41 +1030

    add post handler for add/edit book

--- src/java/servlets/book.java
+++ src/java/servlets/book.java
@@ -22,8 +22,6 @@ import utils.snippets;
  */
 public class book extends HttpServlet {
 
-    // TODO: get max number of books from servlet init param (used in doGet and doPost)
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -37,36 +35,100 @@ public class book extends HttpServlet {
 
         // get admin username from context init param
         String adminUsername = context.getInitParameter("admin");
+        Boolean admin = false;
+        if (adminUsername == username) {
+            admin = true;
+        }
 
         // if no book id, then we're adding a book
         // otherwise, show edit and delete forms
         String bookId = request.getParameter("id");
+        boolean edit = true;
+        if (bookId == null) {
+            edit = false;
+        }
 
         Logger.getLogger(book.class.getName()).log(Level.INFO, String.format("requested book id: %s", bookId));
 
-        // TODO: show different header if admin user
         PrintWriter out = response.getWriter();
         out.println("<html><head>");
         out.println("</head><body>");
-        this.writeHeader(out);
-        snippets.writeLogoutButton(out, base);
+        this.writeHeader(out, edit);
+
+        // links
         out.println("<a href=\"" + base + "/\">book list</a>");
+        snippets.writeLogoutButton(out, base);
+
+        if (admin && edit) {
+            out.println("<p>Editing PLACEHOLDER'S book</p>");
+        }
+
+        // TODO: customize based on number of books already have
         out.printf("<p>Max number of books allowed: %s</p>\n", this.getMaxRecords());
 
-        if (bookId == null) {
-            out.println("<p>Add book</p>");
-        } else {
-            out.println("<p>Edit book</p>");
+        String error = (String) session.getAttribute("error");
+        if (error != null) {
+            out.println("<p>Error: " + error + "</p>");
+            session.removeAttribute("error");
         }
+
+        if (edit) {
+            out.println("<p>Editing book</p>");
+        } else {
+            out.println("<p>Adding book</p>");
+        }
+
+        this.writeEditForm(out, edit);
 
         out.println("</body></html>");
     }
 
-    // TODO: doPost method to handle actually editing
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    private void writeHeader(PrintWriter out) {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+
+        ServletContext context = this.getServletContext();
+        String base = context.getContextPath();
+
+        // get admin username from context init param
+        String adminUsername = context.getInitParameter("admin");
+        Boolean admin = false;
+        if (adminUsername == username) {
+            admin = true;
+        }
+
+        // if no book id, then we're adding a book
+        // otherwise, show edit and delete forms
+        String bookId = request.getParameter("id");
+        boolean edit = true;
+        if (bookId == null) {
+            edit = false;
+        }
+
+        boolean success = true;
+        String errorMessage = "FAIL";
+        // TODO: add/update in database
+        // TODO: use result below
+
+        if (success) {
+            response.sendRedirect(base);
+        } else {
+            session.setAttribute("error", errorMessage);
+            response.sendRedirect(base + "/book?id=" + bookId);
+        }
+
+    }
+
+    private void writeHeader(PrintWriter out, boolean edit) {
         // TODO: tailor header based on action
-        out.println("<h1>Edit/Add entry</h1>");
+        if (edit) {
+            out.println("<h1>Edit Book</h1>");
+        } else {
+            out.println("<h1>Add Book</h1>");
+        }
     }
         
 
@@ -74,5 +136,18 @@ public class book extends HttpServlet {
         ServletConfig config = this.getServletConfig();
         int maxRecords = Integer.parseInt(config.getInitParameter("maxRecords"));
         return maxRecords;
+    }
+
+    private void writeEditForm(PrintWriter out, boolean edit) {
+        String editText;
+        if (edit) {
+            editText = "Update";
+        } else {
+            editText = "Create";
+        }
+
+        out.println("<form action=\"\" method=\"POST\">");
+        out.println("<input type=\"submit\" value=\"" + editText + "\">");
+        out.println("</form>");
     }
 }
