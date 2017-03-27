8b341fbfb098b88465b071d1eedc65b45cef3c46
Samuel Walladge <samuel@swalladge.id.au>
2017-03-27 16:30:02 +1030

    comment and improve snippets

--- src/java/utils/snippets.java
+++ src/java/utils/snippets.java
@@ -8,11 +8,17 @@ package utils;
 import java.io.PrintWriter;
 
 /**
- *
+ * contains some snippets of html that can be reused across various servlets
  * @author Samuel Walladge
  */
 public class snippets {
 
+    /**
+     * writes the logout button (and surrounding stuff) to the printwriter
+     * @param out
+     * @param username username of the currently logged in user
+     * @param base the base url of the app - required for printing links
+     */
     public static void writeLogoutButton(PrintWriter out, String username, String base) {
         out.printf("<div><span>(%s)</span>", username);
         out.println("<form style=\"display:inline;\" action=\"" + base + "/logout\" method=\"POST\">");
@@ -20,6 +26,10 @@ public class snippets {
         out.println("</form></div>");
     }
 
+    /**
+     * writes the beginning tags and head of the html document
+     * @param out 
+     */
     public static void writeHead(PrintWriter out) {
         out.println("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\">");
         out.println("<style>table{border-collapse:collapse;}th,td{border:1px solid grey;padding:10px;}</style>");
@@ -27,7 +37,12 @@ public class snippets {
         out.println("<h1>Books Catalogue</h1>");
     }
 
+    /**
+     * writes the end of the document - footer and end tags
+     * @param out 
+     */
     public static void writeEnd(PrintWriter out) {
+        out.println("<p><small>Copyright &copy; 2017 Samuel Walladge</small></p>");
         out.println("</body></html>");
     }
 }
