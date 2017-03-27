0a5377c2ea8d5391221e223b311cfa483062c3ea
Samuel Walladge <samuel@swalladge.id.au>
2017-03-27 16:22:29 +1030

    comment logout servlet

--- src/java/servlets/logout.java
+++ src/java/servlets/logout.java
@@ -11,19 +11,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- *
+ * handles logging out the user
  * @author Samuel Walladge
  */
 public class logout extends HttpServlet {
 
+    /**
+     * logs the user out - invalidates the session and redirects to the login page
+     * this is performed only for a POST request, due to this changing session state
+     * also this avoids accidentally being logged out by a browser pre-fetching page links
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // remove the session value and redirect to the login page
+        // invalidate the session
         HttpSession session = request.getSession();
-        session.removeAttribute("username");
-        session.removeAttribute("isadmin");
+        session.invalidate();
+
         response.sendRedirect("login");
     }
 }
